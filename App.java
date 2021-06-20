cfc0dde17f73a3812f1496618e0f7006428e7ff4
MNJ <nizamrockstar1@gmail.com>
2021-06-20 13:54:02 +0530

    This is App file commit

---
+++ App.java
@@ -0,0 +1,13 @@
+package com.durgasoft.HelloApp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
